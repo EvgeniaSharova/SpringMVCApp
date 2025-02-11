e892b95bb904c9c1631ece9e15b06e89339025d5
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2025-02-11 17:08:30 +0300

    controllers

---
+++ src/main/java/ru/sharova/springcourse/controllers/SecondController.java
@@ -0,0 +1,12 @@
+package ru.sharova.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    private String exit() {
+        return "second/exit";
+    }
+}
